cd465c360b19e424d3b0e60c2d1876d6e38a8590
Mert Korkmaz <mertkorkmaz_2005@hotmail.com>
2023-09-07 12:23:13 +0200

    alles gemaakt behalve de cijfers weghalen bij de woorden

--- TechJam/numbers.php
+++ TechJam/numbers.php
@@ -1,6 +1,30 @@
 <?php
 
 for($i= 33; $i <= 543; $i++){
-    echo $i .  "<br>";
+    if($i % 2 == 0){
+        echo "<b>$i</b> <br>";
+        //als het getal even is maak het dan dikgerukt 
+    }
+    elseif($i % 5 == 0){
+        echo "Software";
+        // als het getal kan delen door 5 maak er software van
+    }
+   
+    elseif($i % 9 == 0){
+        echo "Developer";
+        //als het getal kan delen door 9 maak er dan developer van
+    }
+    elseif($i % 5 & $i % 9 == 0){
+        echo "Web";
+        // als je deelt door 5 en deelt door 9 en op 0 uitkomt 
+    }
+    else{
+        echo $i .  "<br>";
+        // laat de cijfer zien
+
+    }
+
+    
 }
+
 ?>
