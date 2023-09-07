d3e47b35f57e0291aafc81acf46a4abbf94b7d67
Mert Korkmaz <mertkorkmaz_2005@hotmail.com>
2023-09-07 12:33:21 +0200

    alles werkt

--- TechJam/numbers.php
+++ TechJam/numbers.php
@@ -5,19 +5,20 @@ for($i= 33; $i <= 543; $i++){
         echo "<b>$i</b> <br>";
         //als het getal even is maak het dan dikgerukt 
     }
+    elseif($i % 5 == 0 && $i % 9 == 0){
+        echo "Web" . "<br>";;
+        // als je deelt door 5 en deelt door 9 en op 0 uitkomt 
+    }
     elseif($i % 5 == 0){
-        echo "Software";
+        echo "Software" . "<br>";
         // als het getal kan delen door 5 maak er software van
     }
    
     elseif($i % 9 == 0){
-        echo "Developer";
+        echo "Developer" . "<br>";;
         //als het getal kan delen door 9 maak er dan developer van
     }
-    elseif($i % 5 & $i % 9 == 0){
-        echo "Web";
-        // als je deelt door 5 en deelt door 9 en op 0 uitkomt 
-    }
+   
     else{
         echo $i .  "<br>";
         // laat de cijfer zien
